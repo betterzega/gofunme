02e103dd785189faafea90badbf24112487ee508
betterzega <zega@betterment.com>
2019-06-05 17:46:08 -0400

    handle interests controller

--- app/models/handler/list_people.rb
+++ app/models/handler/list_people.rb
@@ -9,7 +9,7 @@ class Handler::ListPeople
 
   def response
     {
-      text: other_words.join(" ")
+      text: response_text
     }
   end
 
@@ -18,6 +18,22 @@ class Handler::ListPeople
   attr_reader :text
 
   def interest_text
-    text.split[1..]
+    text.split.second
+  end
+
+  def response_text
+    if interests.count == 0
+      "Sorry, no one is interested in this topic :("
+    else
+      "The following people have an interest in #{interest_text}: #{usernames}"
+    end
+  end
+
+  def interests
+    @interests ||= Interest.where(name: interest_text)
+  end
+
+  def usernames
+    @usernames ||= interests.map(&:user).map(&:slack_username).to_sentence
   end
 end
