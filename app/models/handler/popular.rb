b76a95fb30e3d33713ca5b2a45e0dd0a873767fa
betterentley <john.bentley@betterment.com>
2019-06-06 17:16:30 -0400

    count distinct user_ids in case there are duplicates

--- app/models/handler/popular.rb
+++ app/models/handler/popular.rb
@@ -22,6 +22,6 @@ class Handler::Popular
   end
 
   def popular_interests
-    @random_interest ||= Interest.group(:name).order("COUNT(Interest.name)").last(10).pluck(:name)
+    @random_interest ||= Interest.group(:name).order("COUNT(DISTINCT Interest.user_id)").last(10).pluck(:name)
   end
 end
