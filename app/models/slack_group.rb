5f96d9002ed460b42ee95d6d94d620a55083a943
cheng <cheng.peng@betterment.com>
2019-06-06 17:03:52 -0400

    Actually saved salck channel id

--- app/models/slack_group.rb
+++ app/models/slack_group.rb
@@ -10,7 +10,7 @@ class SlackGroup < ApplicationRecord
   def create_remote_group
     slack_client = Slack::Client.new
     response = slack_client.channels_create(name: name)
-    slack_channel_id = response["channel"]["id"]
+    self.slack_channel_id = response["channel"]["id"]
     slack_userids = User.where(id: Interest.where(name: name).pluck(:user_id)).pluck(:slack_user_id)
     slack_userids.each do |user_id|
       invite_response = slack_client.channels_invite(channel: slack_channel_id, user: user_id)
