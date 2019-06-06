826c117bed095b47e9601cf9dcdd06cef9e662c4
cheng <cheng.peng@betterment.com>
2019-06-06 15:31:43 -0400

    yolo

--- app/models/slack_group.rb
+++ app/models/slack_group.rb
@@ -10,5 +10,10 @@ class SlackGroup < ApplicationRecord
   def create_remote_group
     slack_client = Slack::Client.new
     response = slack_client.channels_create(name: name)
+    slack_channel_id = response["channel"]["id"]
+    slack_usernames = User.where(id: Interest.where(name: name).pluck(:user_id))
+    slack_usernames.each do |username|
+      slack_client.channels_invite(channel: slack_channel_id, user: username)
+    end
   end
 end
