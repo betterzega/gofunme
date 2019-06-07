781640605333f82b222196ce1adb80b43c575a20
betterzega <zega@betterment.com>
2019-06-07 09:09:02 -0400

    yolo v3

--- app/models/handler/help.rb
+++ app/models/handler/help.rb
@@ -24,8 +24,10 @@ class Handler::Help
   def response_text
     if first_time_user?
       ":happynerd::happynerd: *WELCOME TO HOBBYLOBBY!* :happynerd::happynerd:\nTell us your interests, and we’ll match you with other Bettermenters that have similar interests!\n\n*Here’s how to use it:*\n- Let us know your interests by typing `/hobbylobby add _interest_`.\n- If a slack channel of like-minded people already exists, you’ll be added to it; if it doesn’t, we’ll save your interests, and automatically create a channel for you when 2 other people express the same interests.\n\n*Looking for some inspiration? Try these other fun commands:*\n`/hobbylobby popular` : Get a list of the top interests\n`/hobbylobby latest` : See the latest interests that were added\n`/hobbylobby random` : See random interests\nFor more commands, type `/hobbylobby`\n\n*Have Fun!!*\n"
-    else
+    elsif text.split.first.present?
       "`#{text.split.first}` is *not* a valid command.\n- To add an interest, use `/hobbylobby add _interest_` \n- To see what interests someone has, use `/hobbylobby interests _slackname_` \n- To see who else has the same interest, use `/hobbylobby people _interest_` \n- To see a random interest, use `/hobbylobby random` \n- To see the latest subscribed interests, use `/hobbylobby latest` \n- To see the latest subscribed interests, use `/hobbylobby popular` \n"
+    else
+      "- To add an interest, use `/hobbylobby add _interest_` \n- To see what interests someone has, use `/hobbylobby interests _slackname_` \n- To see who else has the same interest, use `/hobbylobby people _interest_` \n- To see a random interest, use `/hobbylobby random` \n- To see the latest subscribed interests, use `/hobbylobby latest` \n- To see the latest subscribed interests, use `/hobbylobby popular` \n"
     end
   end
 
